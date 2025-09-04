70a16a6ca7d9367ddd8f9fdfadd98ddb095569fd
SayeemZaman <sayeem312@gmail.com>
2025-09-04 12:12:24 +0000

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "slate-gray": "#6B737D",
+        "gunmetal": "#202937",
+        "prime": "#5046E0",
+        "secondary": "#6366F1"
+      }
+    },
   },
   plugins: [],
 };
