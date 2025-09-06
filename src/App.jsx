85d1c32262b64aaeb1cd00ea0e0ece4af095775d
SayeemZaman <sayeem312@gmail.com>
2025-09-06 11:45:39 +0000

    finish Benefits

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Benefits from './components/Benefits';
 
 function App() {
   return (
     <div className="p-[32px]">
       <Header />
       <Hero />
+      <Benefits />
     </div>
   )
 }
