affb2c60e8ba7857aa026c98eaa084db11c7a5b1
SayeemZaman <sayeem312@gmail.com>
2025-09-06 11:58:01 +0000

    add more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ export default {
         "slate-gray": "#6B737D",
         "gunmetal": "#202937",
         "prime": "#5046E0",
-        "secondary": "#6366F1"
+        "secondary": "#6366F1",
+        "tropical-indigo": "#A28BF3",
+        "palatinate-blue": "#463AC7"
       },
       fontFamily: {
         satoshi: ["satoshi", "sans-serif"]
