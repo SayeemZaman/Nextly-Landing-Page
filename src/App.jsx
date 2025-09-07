65c04892f04f7047cd1aa182ac410ba75dc68287
SayeemZaman <sayeem312@gmail.com>
2025-09-07 07:19:14 +0000

    add Video section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
+import Video from "./components/Video";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Hero />
       <Benefits />
+      <Video />
     </div>
   )
 }
---
+++ src/components/Video.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import Head from "./Head";
+
+export default function Video(){
+  return (
+    <section id="video" className="flex flex-col gap-[16px]">
+      <Head
+        head="WATCH A VIDEO"
+        title="Learn how to fullfil your needs"
+        desc={`
+          This section is to highlight a promo or demo video of your product.
+          Analysts says a landing page with video has 3% more conversion rate.
+          So, don't forget to add one. Just like this.
+        `}
+      />
+      <div className="flex justify-center items-center rounded-[16px] mt-[64px] w-full h-[170px] bg-gradient-to-tr from-tropical-indigo to-palatinate-blue">
+        <span className="flex justify-center items-center rounded-full p-[20px] bg-white aspect-square">
+          <FontAwesomeIcon icon={faPlay} size="2x" style={{color: "#6366F1"}} />
+        </span>
+      </div>
+    </section>
+  );
+}
