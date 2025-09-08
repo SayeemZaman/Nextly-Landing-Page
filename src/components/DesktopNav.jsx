3f6672e3e41e47ff9910c74479a062e632b49c32
SayeemZaman <sayeem312@gmail.com>
2025-09-08 17:16:30 +0000

    add DesktopNav for responsiveness

---
+++ src/components/DesktopNav.jsx
@@ -0,0 +1,30 @@
+function NavLink({name}){
+  return (
+    <li>
+      <a href="#" className="text-slate-gray font-medium">{name}</a>
+    </li>
+  );
+};
+
+function NavList(){
+  return (
+    <nav>
+      <ul className="flex gap-[48px]">
+        {data.map((item, i) => (
+          <NavLink key={i} name={item} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const data = ["Product", "Features", "Pricing", "Company", "Blog"];
+
+export default function DesktopNav(){
+  return (
+    <div className="gap-[48px] w-[75%] hidden lg:!flex items-center">
+      <NavList />
+      <button className="bg-prime py-[8px] px-[16px] rounded-[6px] text-white text-center ml-auto">Get Started</button>
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import logo from "../assets/images/logo.svg";
 import MobileNav from "./MobileNav";
+import DesktopNav from "./DesktopNav";
 
 export default function Header(){
   return (
@@ -9,6 +10,7 @@ export default function Header(){
           <img src={logo} alt="Nextly Logo" className="w-[32px]" />
           <p className="text-2xl text-secondary font-satoshi font-bold">Nextly</p>
         </span>
+        <DesktopNav />
       </header>
       <MobileNav />
     </>
